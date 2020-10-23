bfc854455a14e5db81898559083231215bfb79f3
Bartosz Pazdur <b.pazdur@onet.pl>
2020-10-23 22:36:05 +0200

    Added new thing

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const yup = require('yup')
-const morgan = require('morgan')
-const monk = require('monk')
-const {nanoid} = require('nanoid')
-const cors = require('cors')
-const path = require('path')
+import express from 'express'
+import morgan from 'morgan'
+import monk from 'monk'
+import cors from 'cors'
+import * as path from 'path'
+import * as fs from 'fs'
+
 require('dotenv').config()
 
-const app = express()
+export const app = express()
 
 app.use(express.json())
 app.use(morgan('common'))
@@ -15,10 +15,17 @@ app.use(cors())
 app.use(express.static('./public'))
 
 const database = monk(process.env.MONGODB_URI)
-const urls_collection = database.get('urls')
+export const urls_collection = database.get('urls')
 
-
+fs.readdir(path.resolve('./routes/'), (error, files) => {
+    if(error)
+        throw new Error(error)
+    
+    if(files.length === 0)
+        throw new Error('Target directory is empty')
+    
+    files.forEach(file => require(`./routes/${file}`))
+})
 
 const PORT = process.env.PORT || 3000
-
 app.listen(PORT, () => console.log(`Server is listen here -> http://localhost:${PORT}`))
